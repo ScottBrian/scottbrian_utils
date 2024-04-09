4d8792b80173d929cd3f401551a7d22a51244e24
ScottBrian <sbtuttle@outlook.com>
2024-04-09 13:01:22 -0400

    restructure log_ver part 22

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,18 +17,18 @@ from pathlib import Path
 
 proj_dir = Path.cwd().resolve().parents[1]  # back two directories
 sys.path.insert(0, fspath(proj_dir))
-sys.path.insert(0, fspath(proj_dir / 'src'))
-sys.path.insert(0, fspath(proj_dir / 'src/scottbrian_utils'))
+sys.path.insert(0, fspath(proj_dir / "src"))
+sys.path.insert(0, fspath(proj_dir / "src/scottbrian_utils"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'scottbrian_utils'
-copyright = '2020, 2023, Scott Tuttle'
-author = 'Scott Tuttle'
+project = "scottbrian_utils"
+copyright = "2020, 2023, Scott Tuttle"
+author = "Scott Tuttle"
 
 # The full version, including alpha/beta/rc tags
-release = '4.0.0'
+release = "4.0.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -36,16 +36,18 @@ release = '4.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx_autodoc_typehints',
-              'sphinx_rtd_theme']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx_rtd_theme",
+]
 
-autoclass_content = 'both'
-autodoc_member_order = 'groupwise'
+autoclass_content = "both"
+autodoc_member_order = "groupwise"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -58,9 +60,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
